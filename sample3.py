4eb899465e8305c1371c0ebbbbb96e00fac08e10
Akinpelu Adedamola Basit <adedamolabasit09@gmail.com>
2019-10-12 07:58:29 +0100

    another change

--- sample3.py
+++ sample3.py
@@ -7,16 +7,20 @@ class Salary:
         return (self.pay*12)+self.bonus
 
 
-class Employee:
-    def __init__(self,name,age,pay,bonus):
+class Employee(Salary):
+    def __init__(self,name,age,Salary):
         self.name=name
         self.age=age
-        self.obj_salary=Salary(pay,bonus)
+        self.obj_salary=Salary(pay,bonus);
+
+    def credit_alert(self):
+        avl_bal=0
+        avl_bal+=self.pay
+        print(f'your account has been credited with #{avl_bal} ')
 
 worker=Salary(500000,250000)
-all=Employee('adedamola',21,80000,7437)
+all=Employee('adedamola',21,80000,7437,worker)
 print(worker.annual_salary())
-print(worker.bonus)
 print(all.obj_salary.pay)
 
 
