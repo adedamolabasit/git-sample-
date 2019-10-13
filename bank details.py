e7b3bd803e50a117c01600603da367ab00653e29
Akinpelu Adedamola Basit <adedamolabasit09@gmail.com>
2019-10-13 09:29:55 +0100

    another change

---
+++ bank details.py
@@ -0,0 +1,9 @@
+class BankAccount:
+    def __init__(self,name,DOB,BVN,location,bank):
+        self.name=name
+        self.DOB=DOB
+        self.BVN=BVN
+        self.location=location
+        self.bank=bank
+
+    def account_name(self,phone_no,):
